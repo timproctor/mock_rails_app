e4c339f97557d5e9fa47f917760c8ef651d3bda5
timproctor <timandcamiproctor@gmail.com>
2016-06-03 12:46:25 -0600

    Added bcrypt gem for User login or out.

--- flix/db/schema.rb
+++ flix/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20121217184756) do
+ActiveRecord::Schema.define(version: 20160603184228) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -37,5 +37,13 @@ ActiveRecord::Schema.define(version: 20121217184756) do
   end
 
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
