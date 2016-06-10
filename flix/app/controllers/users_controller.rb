3022e79e1c0526f9a06acc97070d5c40f57479d3
timproctor <timandcamiproctor@gmail.com>
2016-06-10 10:16:29 -0600

    Set up show view with passing spec.

--- flix/app/controllers/users_controller.rb
+++ flix/app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- flix/spec/features/show_user_spec.rb
+++ flix/spec/features/show_user_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe "Viewing a user's profile page" do
+
   it "shows the user's details" do
     user = User.create!(user_attributes)
 
-    visit users_url(user)
+    visit user_url(user)
 
     expect(page).to have_text(user.name)
     expect(page).to have_text(user.email)
   end
+
 end
