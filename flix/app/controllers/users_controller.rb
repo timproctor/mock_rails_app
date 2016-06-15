844887f59f57d6eb7ee16743fa770177de223110
timproctor <timandcamiproctor@gmail.com>
2016-06-15 09:32:35 -0600

    Create action for user.

--- flix/app/controllers/users_controller.rb
+++ flix/app/controllers/users_controller.rb
@@ -12,9 +12,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "Thanks, #{@user.name} for signing up."
+    else
+      render :new
+    end
+  end
+
 private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
