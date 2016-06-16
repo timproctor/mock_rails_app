d7c61cd1c21d746c96346a64c20e6a0055c38de4
timproctor <timandcamiproctor@gmail.com>
2016-06-16 09:09:18 -0600

    Destroy action and delete user specs passing.

--- flix/spec/features/delete_user_spec.rb
+++ flix/spec/features/delete_user_spec.rb
@@ -9,7 +9,7 @@ describe "Delete a user" do
     click_link 'Delete Account'
 
     expect(current_path).to eq(root_path)
-    expect (page).to have_text('Account successfully deleted!')
+    expect(page).to have_text('Account successfully deleted!')
 
     visit users_path
 
